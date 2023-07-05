c0e5fa377c02f619c86108a4f6f2b0761c0bc75d
svillamizar <ingsandravillamizar@github.com>
2023-07-05 08:10:56 -0500

    mejora conexion base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into productos (codigo, descripcion, nombres) values ("001",'TVLG X20 ULTRA','LGTV20');
+insert into productos (codigo, descripcion, nombres) values ("002",'TVLG X30 ULTRA','LGTV30');
+insert into productos (codigo, descripcion, nombres) values ("003",'TVLG X40 ULTRA','LGTV40');
+insert into productos (codigo, descripcion, nombres) values ("004",'TVLG X50 ULTRA','LGTV50');
+insert into productos (codigo, descripcion, nombres) values ("005",'TVLG X60 ULTRA','LGTV60');
+insert into productos (codigo, descripcion, nombres) values ("006",'TVLG X70 ULTRA','LGTV70');
